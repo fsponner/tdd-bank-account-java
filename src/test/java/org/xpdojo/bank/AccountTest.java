ebfebc03e6aa4339f8d6f90ad6640634d4b90b87
Flo <florian.sponner@googlemail.com>
2020-07-16 16:54:43 +0200

    another first try

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
     }
+
+    @Test
+    public void depositTenToInitialAccount(){
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void depositTwiceToInitialAccount(){
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
+
 }
