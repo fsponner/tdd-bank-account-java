483d17242d61a3e5b632807096b129e2e1eefa0c
Flo <florian.sponner@googlemail.com>
2020-07-30 00:03:16 +0200

    done

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,15 +1,74 @@
 package org.xpdojo.bank;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Account {
 
-    private int balance = 0;
+    private int balance;
+    private final List<Object[]> statement;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+    public Account(int amount) {
+        this.balance += amount;
+        this.statement = new ArrayList<>();
+    }
+
+    public Account() {
+        this.balance = 0;
+        this.statement = new ArrayList<>();
+    }
 
     public int balance() {
-        return balance;
+        return this.balance;
     }
 
     public void deposit(int amount) {
-        balance += amount;
+        this.balance += amount;
+        addStatementEntry("Deposit", amount);
     }
 
+    public void withdraw(int amount) {
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficiant account balance!");
+        }
+        this.balance -= amount;
+        addStatementEntry("Withdrawal", amount);
+    }
+
+    public Account transfer(Account to, int amount) {
+        //Objects.requireNonNull(to, "account must not be null");
+        this.withdraw(amount);
+        to.deposit(amount);
+        return to;
+    }
+
+    public String balanceSlip() {
+        Date date = new Date();
+        return dateFormat.format(date) + " " + balance;
+    }
+
+    public void addStatementEntry(String type, int amount) {
+        Date date = new Date();
+        Object[] entry = new Object[3];
+        entry[0] = type;
+        entry[1] = amount;
+        entry[2] = dateFormat.format(date);
+        statement.add(entry);
+    }
+
+    public List<Object[]> statement(){
+        return statement;
+    }
+
+    public ArrayList<Object[]> filteredStatement(String type) {
+        ArrayList<Object[]> filteredStatement;
+        filteredStatement = new ArrayList<>();
+        for(Object[] entry:statement){
+            if(entry[0].equals(type)){
+                filteredStatement.add(entry);
+            }
+        }
+        return filteredStatement;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,31 +1,127 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountTest {
+public class AccountTest{
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     @Test
-    public void newAccountShouldHaveZeroBalance() {
+    public void newAccountWithoutBalance(){
         Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
 
     @Test
-    public void depositTenToInitialAccount(){
+    public void newAccountWithTenBalance(){
+        Account account = new Account(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void depositTenToEmptyAccount(){
         Account account = new Account();
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
 
     @Test
-    public void depositTwiceToInitialAccount(){
+    public void depositTenAndTwentyToEmptyAccount(){
         Account account = new Account();
         account.deposit(10);
         account.deposit(20);
         assertThat(account.balance()).isEqualTo(30);
     }
 
+    @Test
+    public void withdrawTenAndTwentyFromAccount() {
+        Account account = new Account(30);
+        account.withdraw(10);
+        account.withdraw(20);
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void depositAndWithdrawTenToInitialAccount() {
+        Account account = new Account();
+        account.deposit(10);
+        account.withdraw(10);
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void withdrawNegativeAccountBalanceException(){
+        Account account = new Account();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> account.withdraw(10));
+    }
+
+    @Test
+    public void transferTenBetweenAccounts(){
+        Account from = new Account(10);
+        Account to = new Account(0);
+        to = from.transfer(to, 10);
+        assertThat(from.balance()).isEqualTo(0);
+        assertThat(to.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void getAccountBalanceSlip(){
+        Account account = new Account();
+        Date date = new Date();
+        assertThat(account.balanceSlip()).isEqualTo(dateFormat.format(date) + " " + account.balance());
+    }
+
+    @Test
+    public void addStatementEntryToStatement(){
+        Account account = new Account();
+        account.addStatementEntry("DEPOST",10);
+    }
+
+    @Test
+    public void getEmptyAccountStatement(){
+        Account account = new Account();
+        assertThat(account.statement().isEmpty()).isEqualTo(true);
+    }
+
+    @Test
+    public void getAccountStatementWithOneEntry(){
+        Account account = new Account();
+        Date date = new Date();
+        account.addStatementEntry("Deposit", 10);
+        assertThat(account.statement().size()).isEqualTo(1);
+        assertThat(account.statement().get(0)[0]).isEqualTo("Deposit");
+        assertThat(account.statement().get(0)[1]).isEqualTo(10);
+        assertThat(account.statement().get(0)[2]).isEqualTo(dateFormat.format(date));
+        assertThat(account.balanceSlip()).isEqualTo(dateFormat.format(date) + " " + account.balance());
+
+    }
+
+    @Test
+    public void getStatementAfterDeposit(){
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.statement().size()).isEqualTo(1);
+        assertThat(account.statement().get(0)[0]).isEqualTo("Deposit");
+        assertThat(account.statement().get(0)[1]).isEqualTo(10);
+    }
+
+    @Test
+    public void getFilteredStatement(){
+        Account account = new Account();
+        Account transferAccount = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        account.withdraw(5);
+        account.transfer(transferAccount, 5);
+        assertThat(account.filteredStatement("Withdrawal").size()).isEqualTo(2);
+        for (Object[] entry : account.filteredStatement("Withdrawal")) {
+            assertThat(entry[0]).isEqualTo("Withdrawal");
+        }
+    }
 }
